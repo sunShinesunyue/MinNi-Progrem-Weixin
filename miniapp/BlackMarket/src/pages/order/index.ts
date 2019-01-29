cfdf3e5fa4fe6eb0c3a54a02e04f30c913a82b2c
sunyue <814983809>
2019-01-29 14:06:09 +0800

    order页面

---
+++ miniapp/BlackMarket/src/pages/order/index.ts
@@ -0,0 +1,29 @@
+import { Vue, Component } from 'vue-property-decorator'
+
+// 1.从vuex里面引入mapState,mapActions
+import {mapStata,mapActions} from 'vuex'
+
+@Component({
+    // 2.计算属性
+    computed: {
+        // 3.展开
+        ...mapState({
+            // 4.state['order']这样的写法
+            list :state => state['order'].list
+        })
+    },
+    // 5.方法
+    methods: {
+        ...mapActions({
+          getOrderList: 'order/getOrderList'
+        })
+    }
+    
+})
+
+// 6.写法
+export default class extends Vue {
+    onShow() {
+        this['getOrderList'](this.$root.$mp.query)
+    }
+}
---
+++ miniapp/BlackMarket/src/pages/order/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        我是order页面
+    </div>
+</template>
+
+<script lang="ts" src="./index.ts">
+</script>
+
+<style lang="less" scoped>
+    
+</style>
